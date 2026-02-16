a78d456f1a386685faff8aea9ede4cc3046c640b
manrooh228 <maksatsuper57915@gmail.com>
2026-02-16 14:45:22 +0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    [Header("Movement Details")]
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = moveInput.normalized * moveSpeed;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# StrangeHaze
+2D Top-down game created for Brackeys Game Jam 2026.1
