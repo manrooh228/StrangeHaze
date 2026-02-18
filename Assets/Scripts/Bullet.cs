d5f907e7ca83d31714fb720d6ae9a5fa1d4148ec
manrooh228 <maksatsuper57915@gmail.com>
2026-02-19 02:38:48 +0500

    Bullet destroy time

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _timeToDestroy;
 
+    private float _time;
     private void Update()
     {
+        _time += Time.deltaTime;
+        if (_time > _timeToDestroy)
+        {
+            Destroy(gameObject);
+        }
         transform.Translate(Vector2.right * _speed * Time.deltaTime); 
     }
 }
